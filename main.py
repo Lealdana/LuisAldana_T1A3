f512f612de2feaa691ae205d9726db99221af983
Lealdana <wdluisaldana@gmail.com>
2023-05-03 12:39:29 +1000

    Creation of app terminal, first layout

---
+++ main.py
@@ -0,0 +1,20 @@
+questions = ("What is the name of the largest mammal in the world?: ",
+
+             "What is the name of the famous dog from the comic strip 'Peanuts?': ",
+             "Which planet in our solar system is known as the 'Red Planet'?",
+             " Who is the famous fictional detective created by Sir Arthur Conan Doyle?",
+             "What is the name of the wizard in the Harry Potter series?",
+             "What is the name of the highest mountain in the world?",
+             "What is the name of the famous cartoon character who is always chasing a bird?",
+             "What is the name of the most famous movie about a giant gorilla?",
+             "What is the name of the famous animated movie about a clownfish searching for his son?",
+             "What is the name of the famous song by Queen that starts with the lyrics 'Is this the real life? Is this just fantasy?'"
+
+             )
+
+options = ((), (), (), (), (), (), (), (), (), ())
+
+answers = ()
+guesses = ()
+score = 0
+question_num()
